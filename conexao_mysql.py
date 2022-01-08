44cd9de9a863c12daabffe54565b970525ce6917
Scrypterz <87875138+Scrypterz@users.noreply.github.com>
2022-01-08 13:42:38 -0300

    Update conexao_mysql.py

--- conexao_mysql.py
+++ conexao_mysql.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+#Efetuar a conexão com o DB
 banco = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -7,18 +8,19 @@ banco = mysql.connector.connect(
     database='teste'
 )
 
-
+#Criar o banco de dados
 cursor = banco.cursor()
 cursor.execute('CREATE DATABASE teste')
 
-
-
+#Criar a tabale do banco de dados
 cursor = banco.cursor()
 cursor.execute('CREATE TABLE pessoas (nome VARCHAR(255), idade INT (3), email VARCHAR (255))')
 
+#Inserir dados na tabela
 comando_SQL = 'INSERT INTO pessoas (nome,idade,email) VALUES (%s,%s,%s)'
 dados = ('Matheus, 20, matheus@gmail.com')
 
+#Enviar os dados
 cursor = banco.cursor()
 cursor.execute(comando_SQL, dados)
 banco.commit()
